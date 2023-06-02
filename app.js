6b8290b8ec10e9a12528de1bab82126d95945cc9
Martin H <martinhj.sama@gmail.com>
2023-06-01 23:56:18 -0500

    Finished

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const btnHide = document.querySelectorAll('#hideList');
 const list = document.querySelectorAll('#list-items');
 
-btnHide.forEach((btn, index)=>{
-    btn.addEventListener('click',()=>{
-        list[index].classList.toggle('active')
-    })
-})
-
-
-
+btnHide.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      if (window.innerWidth <= 768) {
+        list[index].classList.toggle('active');
+      }
+    });
+  });
+  
+  let screenWidth = window.innerWidth;
+  if (screenWidth > 768) {
+    btnHide.forEach(btn => {
+      btn.removeAttribute('id');
+    });
+  }
